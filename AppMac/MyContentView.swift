d87ed229cc66571ca7b94287f631b6b423644ed5
Simon Strandgaard <neoneye@gmail.com>
2020-02-18 17:01:29 +0100

    MyContentView now gets its data from an ObservableObject.

--- AppMac/MyContentView.swift
+++ AppMac/MyContentView.swift
@@ -3,28 +3,35 @@ import Cocoa
 import SpriteKit
 import SwiftUI
 import SnakeGame
+import Combine
 
 class MyHostingController: NSHostingController<MyContentView> {
     @objc required dynamic init?(coder: NSCoder) {
-        super.init(coder: coder, rootView: MyContentView())
+        super.init(coder: coder, rootView: MyContentView(model: MyObservable()))
     }
 }
 
+class MyObservable: ObservableObject {
+    @Published var player1Info = "Player 1 (green)\nAlive\nLength 29"
+    @Published var player2Info = "Player 2 (blue)\nDead by collision with wall\nLength 14"
+}
+
 struct MyContentView: View {
+    @ObservedObject var model: MyObservable
     var isPreview: Bool = false
 
     var body: some View {
         VStack(spacing: 1) {
             SpriteKitContainer(isPreview: isPreview)
             HStack(spacing: 1) {
-                Text("Player 1 (green)\nAlive\nLength 29")
+                Text(model.player1Info)
                     .padding(10)
                     .frame(minWidth: 80, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                .background(Color.green)
-                Text("Player 2 (blue)\nDead by collision with wall\nLength 14")
+                    .background(Color.green.saturation(0.7))
+                Text(model.player2Info)
                     .padding(10)
 .frame(minWidth: 80, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                .background(Color.blue)
+                    .background(Color.blue.saturation(0.7))
             }
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 80, maxHeight: 100)
         }
@@ -35,10 +42,14 @@ struct MyContentView: View {
 struct ContentView_Previews : PreviewProvider {
 
     static var previews: some View {
-        Group {
-            MyContentView(isPreview: true).previewLayout(.fixed(width: 130, height: 200))
-            MyContentView(isPreview: true).previewLayout(.fixed(width: 300, height: 200))
-            MyContentView(isPreview: true).previewLayout(.fixed(width: 400, height: 150))
+        let model = MyObservable()
+        return Group {
+            MyContentView(model: model, isPreview: true)
+                .previewLayout(.fixed(width: 130, height: 200))
+            MyContentView(model: model, isPreview: true)
+                .previewLayout(.fixed(width: 300, height: 200))
+            MyContentView(model: model, isPreview: true)
+                .previewLayout(.fixed(width: 400, height: 150))
         }
     }
 
