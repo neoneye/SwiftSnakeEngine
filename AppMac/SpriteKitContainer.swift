453db39a1e241d5b370f9cc073a7923e56b65af7
Simon Strandgaard <neoneye@gmail.com>
2020-02-20 18:21:21 +0100

    Don't clear the player info while previewing.

--- AppMac/SpriteKitContainer.swift
+++ AppMac/SpriteKitContainer.swift
@@ -23,10 +23,12 @@ struct SpriteKitContainer: NSViewRepresentable {
         func sendInfoEvent(_ event: SnakeGameInfoEvent) {
             switch event {
             case .showLevelSelector:
-                parent.player1Info = ""
-                parent.player2Info = ""
-                parent.player1Length = 0
-                parent.player2Length = 0
+                if !parent.isPreview {
+                    parent.player1Info = ""
+                    parent.player2Info = ""
+                    parent.player1Length = 0
+                    parent.player2Length = 0
+                }
             case let .beginNewGame(gameState):
                 parent.player1Info = ""
                 parent.player2Info = ""
