4742a80885502a83723d519ccbc7bc9b7985de14
Simon Strandgaard <neoneye@gmail.com>
2020-03-03 13:10:41 +0100

    SnakeHeadDirection: testing rotatedCCW and rotatedCW.

--- SnakeGameTests/T2000_SnakeHeadDirection.swift
+++ SnakeGameTests/T2000_SnakeHeadDirection.swift
@@ -8,4 +8,50 @@ class T2000_SnakeHeadDirection: XCTestCase {
         let actual: String = directions.map { "\($0)" }.joined(separator: " ")
         XCTAssertEqual(actual, "↑ ← → ↓")
     }
+
+    func test1_rotatedCCW() {
+        do {
+            let actual = SnakeHeadDirection.up.rotatedCCW
+            XCTAssertEqual(actual, .left)
+        }
+        do {
+            let actual = SnakeHeadDirection.up.rotatedCCW.rotatedCCW
+            XCTAssertEqual(actual, .down)
+        }
+        do {
+            let actual = SnakeHeadDirection.left.rotatedCCW.rotatedCCW
+            XCTAssertEqual(actual, .right)
+        }
+        do {
+            let actual = SnakeHeadDirection.up.rotatedCCW.rotatedCCW.rotatedCCW.rotatedCCW
+            XCTAssertEqual(actual, .up)
+        }
+        do {
+            let actual = SnakeHeadDirection.left.rotatedCCW.rotatedCCW.rotatedCCW.rotatedCCW
+            XCTAssertEqual(actual, .left)
+        }
+    }
+
+    func test2_rotatedCW() {
+        do {
+            let actual = SnakeHeadDirection.up.rotatedCW
+            XCTAssertEqual(actual, .right)
+        }
+        do {
+            let actual = SnakeHeadDirection.up.rotatedCW.rotatedCW
+            XCTAssertEqual(actual, .down)
+        }
+        do {
+            let actual = SnakeHeadDirection.left.rotatedCW.rotatedCW
+            XCTAssertEqual(actual, .right)
+        }
+        do {
+            let actual = SnakeHeadDirection.up.rotatedCW.rotatedCW.rotatedCW.rotatedCW
+            XCTAssertEqual(actual, .up)
+        }
+        do {
+            let actual = SnakeHeadDirection.left.rotatedCW.rotatedCW.rotatedCW.rotatedCW
+            XCTAssertEqual(actual, .left)
+        }
+    }
 }
