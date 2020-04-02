69111bd90c4dbee2ccb842499f0a3bda1f48cefb
Simon Strandgaard <neoneye@gmail.com>
2020-04-02 23:25:18 +0200

    Disabled graphviz export for now.

--- SnakeGame/Bot/SnakeBot6.swift
+++ SnakeGame/Bot/SnakeBot6.swift
@@ -20,7 +20,7 @@ public class SnakeBot6: SnakeBot {
 		)
 	}
 
-    private let debug_graphvizExport = true
+    private let debug_graphvizExport = false
 	private let iteration: UInt
 	private let previousIterationData: PreviousIterationData?
 
