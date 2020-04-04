1fc0d1f793fcb65a56fd38b7850fb3ee0f9aafc5
Simon Strandgaard <neoneye@gmail.com>
2020-04-04 20:20:36 +0200

    Added FloatFormatting from my TriangleDraw repo.

---
+++ SnakeGame/Common/FloatFormatting.swift
@@ -0,0 +1,63 @@
+// MIT license. Copyright (c) 2020 Simon Strandgaard. All rights reserved.
+import Foundation
+import CoreGraphics
+
+extension CGFloat {
+	public var string0: String {
+		return String(format: "%.0f", self)
+	}
+	public var string1: String {
+		return String(format: "%.1f", self)
+	}
+	public var string2: String {
+		return String(format: "%.2f", self)
+	}
+}
+
+extension Float {
+	public var string0: String {
+		return String(format: "%.0f", self)
+	}
+	public var string1: String {
+		return String(format: "%.1f", self)
+	}
+	public var string2: String {
+		return String(format: "%.2f", self)
+	}
+}
+
+extension Double {
+	public var string0: String {
+		return String(format: "%.0f", self)
+	}
+	public var string1: String {
+		return String(format: "%.1f", self)
+	}
+	public var string2: String {
+		return String(format: "%.2f", self)
+	}
+}
+
+extension CGPoint {
+	public var string0: String {
+		return "(\(self.x.string0), \(self.y.string0))"
+	}
+	public var string1: String {
+		return "(\(self.x.string1), \(self.y.string1))"
+	}
+	public var string2: String {
+		return "(\(self.x.string2), \(self.y.string2))"
+	}
+}
+
+extension CGSize {
+	public var string0: String {
+		return "(\(self.width.string0), \(self.height.string0))"
+	}
+	public var string1: String {
+		return "(\(self.width.string1), \(self.height.string1))"
+	}
+	public var string2: String {
+		return "(\(self.width.string2), \(self.height.string2))"
+	}
+}
