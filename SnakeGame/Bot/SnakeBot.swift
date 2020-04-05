a93d02e3ded01590cfe4d22145870be62cf9dd64
Simon Strandgaard <neoneye@gmail.com>
2020-04-06 00:43:19 +0200

    Added comments

--- SnakeGame/Bot/SnakeBot.swift
+++ SnakeGame/Bot/SnakeBot.swift
@@ -10,7 +10,13 @@ public protocol SnakeBot: class {
 	static var info: SnakeBotInfo { get }
 	init()
 	func compute(level: SnakeLevel, player: SnakePlayer, oppositePlayer: SnakePlayer, foodPosition: IntVec2?) -> SnakeBot
+
+    /// The first element is the current head position
+    /// The second element is be the first computed position (near future)
+    /// The following elements have lower and lower confidence (far out future).
     var plannedPath: [IntVec2] { get }
+
+    /// The computed move for the player to take
     var plannedMovement: SnakeBodyMovement { get }
 }
 
