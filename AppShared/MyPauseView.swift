20fcaef86f4fe15eb05ed99b8cac6af82ecafccd
Simon Strandgaard <neoneye@gmail.com>
2020-04-11 20:17:16 +0200

    Preparing an "Exit Game" button, that takes the user from the "ingame menu" to the "level selector".

--- AppShared/MyPauseView.swift
+++ AppShared/MyPauseView.swift
@@ -1,17 +1,48 @@
 // MIT license. Copyright (c) 2020 Simon Strandgaard. All rights reserved.
 import SwiftUI
 
+#if os(iOS)
+import EngineIOS
+#elseif os(macOS)
+import EngineMac
+#else
+#error("Unknown OS")
+#endif
+
 struct MyPauseView: View {
     @Binding var presentedAsModal: Bool
 
     #if os(iOS)
+
+    var contentArea: some View {
+        VStack(spacing: 20) {
+            HStack(spacing: 40) {
+                Text("Human\nLength 14")
+                Text("Computer\nLength 18")
+            }
+
+            Spacer()
+
+            Button(action: {
+                log.debug("exit")
+            }) {
+                Text("Exit Game")
+                    .foregroundColor(.white)
+                    .padding(.all)
+            }
+            .background(Color.red)
+            .cornerRadius(5)
+        }
+        .padding(EdgeInsets(top: 40, leading: 0, bottom: 40, trailing: 0))
+    }
+
     var body: some View {
         NavigationView {
-            Text("Content")
+            contentArea
                 .navigationBarTitle("Game Paused")
                 .navigationBarItems(leading:
                     Button("Continue") { self.presentedAsModal = false }
-            )
+                )
         }
     }
     #else
