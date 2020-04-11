6c914a3a42f5d9e925dae48fdca0f257e4fb138d
Simon Strandgaard <neoneye@gmail.com>
2020-04-11 19:39:39 +0200

    PauseView now with title.

--- AppShared/MyPauseView.swift
+++ AppShared/MyPauseView.swift
@@ -4,9 +4,21 @@ import SwiftUI
 struct MyPauseView: View {
     @Binding var presentedAsModal: Bool
 
+    #if os(iOS)
+    var body: some View {
+        NavigationView {
+            Text("Content")
+                .navigationBarTitle("Game Paused")
+                .navigationBarItems(leading:
+                    Button("Continue") { self.presentedAsModal = false }
+            )
+        }
+    }
+    #else
     var body: some View {
         Button("Continue") { self.presentedAsModal = false }
     }
+    #endif
 }
 
 struct MyPauseView_Previews: PreviewProvider {
