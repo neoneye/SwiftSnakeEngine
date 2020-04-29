d2277794e45941bef88fe4dfe1aaa556e0ee5363
Simon Strandgaard <neoneye@gmail.com>
2020-04-29 23:32:47 +0200

    Extracted preventHumanCollisions() function.

--- EngineShared/Snake/SnakeGameExecuter.swift
+++ EngineShared/Snake/SnakeGameExecuter.swift
@@ -322,40 +322,6 @@ public class SnakeGameExecuterInteractive: SnakeGameExecuter {
 }
 
 extension SnakeGameState {
-
-	/// Checks the human inputs and prevent humans from colliding with walls/snakes
-	public func preventHumanCollisions() -> SnakeGameState {
-		var gameState: SnakeGameState = self
-
-		if gameState.player1.role == .human && gameState.player1.isAlive && gameState.player1.pendingMovement != .dontMove {
-			let detector = SnakeCollisionDetector.create(
-				level: gameState.level,
-				foodPosition: gameState.foodPosition,
-				player1: gameState.player1,
-				player2: gameState.player2
-			)
-			detector.process()
-			if detector.player1Alive == false {
-                log.info("player1 will collide with something. \(detector.collisionType1). Preventing this movement.")
-				gameState = gameState.updatePendingMovementForPlayer1(.dontMove)
-			}
-		}
-		if gameState.player2.role == .human && gameState.player2.isAlive && gameState.player2.pendingMovement != .dontMove {
-			let detector = SnakeCollisionDetector.create(
-				level: gameState.level,
-				foodPosition: gameState.foodPosition,
-				player1: gameState.player1,
-				player2: gameState.player2
-			)
-			detector.process()
-			if detector.player2Alive == false {
-                log.info("player2 will collide with something. \(detector.collisionType2). Preventing this movement.")
-				gameState = gameState.updatePendingMovementForPlayer2(.dontMove)
-			}
-		}
-		return gameState
-	}
-
 	public func isWaitingForHumanInput() -> Bool {
 		var waitingForPlayers = false
 		if self.player1.isAlive && self.player1.pendingMovement == .dontMove {
