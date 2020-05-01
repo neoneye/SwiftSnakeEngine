2895bd6d5aa43201f2fbbd9372540e5ac34c53a0
Simon Strandgaard <neoneye@gmail.com>
2020-05-02 01:35:53 +0200

    Extracted SnakePlayerRole enum.

--- EngineShared/Snake/SnakePlayer.swift
+++ EngineShared/Snake/SnakePlayer.swift
@@ -1,31 +1,9 @@
 // MIT license. Copyright (c) 2020 Simon Strandgaard. All rights reserved.
 import Foundation
 
-public enum SnakePlayerRole {
-	case none
-	case human
-	case bot(snakeBotType: SnakeBot.Type)
-}
-
 public enum SnakePlayerId {
     case player1
     case player2
-}
-
-extension SnakePlayerRole: Equatable {
-	public static func == (lhs: SnakePlayerRole, rhs: SnakePlayerRole) -> Bool {
-		switch (lhs, rhs) {
-		case (.none, .none):
-			return true
-		case (.human, .human):
-			return true
-		case let (.bot(bot0), .bot(bot1)):
-			let isEqual_snakeBotType: Bool = bot0 == bot1
-			return isEqual_snakeBotType
-		default:
-			return false
-		}
-	}
 }
 
 public class SnakePlayer {
