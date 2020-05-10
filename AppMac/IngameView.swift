6d0eefd1d5e5a973dfcb0473be57924ba1cca8fd
Simon Strandgaard <neoneye@gmail.com>
2020-05-10 13:49:16 +0200

    LevelView now takes a level parameter, instead of using a hardcoded level.

--- AppMac/IngameView.swift
+++ AppMac/IngameView.swift
@@ -10,30 +10,48 @@ import EngineMac
 #endif
 
 struct IngameView: View {
+    let level: SnakeLevel
+
     var body: some View {
         ZStack {
-            LevelView()
+            self.backgroundGradient
+                .edgesIgnoringSafeArea(.all)
+
+            LevelView(level: level)
 
             snakePathView()
         }
     }
 
-    func snakePathView() -> some View {
+    private var backgroundGradient: LinearGradient {
+        let gradient = Gradient(colors: [
+            Color(red: 192/255.0, green: 192/255.0, blue: 192/255.0),
+            Color(red: 50/255.0, green: 50/255.0, blue: 50/255.0)
+        ])
+        return LinearGradient(gradient: gradient,
+                              startPoint: .top,
+                              endPoint: .bottom)
+    }
+
+    private func snakePathView() -> some View {
         let positions: [IntVec2] = [
-            IntVec2(x:  7, y: 10),
-            IntVec2(x: 10, y: 10),
-            IntVec2(x: 10, y:  7),
+            IntVec2(x:  7, y:  8),
+            IntVec2(x: 10, y:  8),
+            IntVec2(x: 10, y:  5),
+            IntVec2(x: 13, y:  5),
             IntVec2(x: 13, y:  7),
-            IntVec2(x: 13, y:  9),
-            IntVec2(x: 12, y:  9),
-            IntVec2(x: 12, y: 12),
+            IntVec2(x: 12, y:  7),
+            IntVec2(x: 12, y: 10),
         ]
-        return SnakePathView(rowCount: 20, columnCount: 15, positions: positions)
+        return SnakePathView(rowCount: UInt(level.size.y), columnCount: UInt(level.size.x), positions: positions)
     }
 }
 
 struct IngameView_Previews: PreviewProvider {
     static var previews: some View {
-        IngameView()
+        let uuid = UUID(uuidString: "cdeeadf2-31c9-48f4-852f-778b58086dd0")!
+        let level: SnakeLevel = SnakeLevelManager.shared.level(id: uuid)!
+
+        return IngameView(level: level)
     }
 }
--- AppMac/LevelView.swift
+++ AppMac/LevelView.swift
@@ -52,22 +52,8 @@ struct LevelGridView: View {
 
     var body: some View {
         GeometryReader { geometry in
-            ZStack {
-                self.backgroundGradient
-                    .edgesIgnoringSafeArea(.all)
-                self.gridView(geometry)
-            }
+            self.gridView(geometry)
         }
-    }
-
-    private var backgroundGradient: LinearGradient {
-        let gradient = Gradient(colors: [
-            Color(red: 192/255.0, green: 192/255.0, blue: 192/255.0),
-            Color(red: 50/255.0, green: 50/255.0, blue: 50/255.0)
-        ])
-        return LinearGradient(gradient: gradient,
-                              startPoint: .top,
-                              endPoint: .bottom)
     }
 
     private func gridView(_ geometry: GeometryProxy) -> some View {
@@ -83,9 +69,9 @@ struct LevelGridView: View {
 }
 
 struct LevelView: View {
+    let level: SnakeLevel
+
     var body: some View {
-        let uuid = UUID(uuidString: "cdeeadf2-31c9-48f4-852f-778b58086dd0")!
-        let level: SnakeLevel = SnakeLevelManager.shared.level(id: uuid)!
         let size: UIntVec2 = level.size
         let levelCellArray: [LevelCell] = LevelCell.create(level: level)
 
@@ -95,12 +81,15 @@ struct LevelView: View {
 
 struct LevelView_Previews: PreviewProvider {
     static var previews: some View {
+        let uuid = UUID(uuidString: "cdeeadf2-31c9-48f4-852f-778b58086dd0")!
+        let level: SnakeLevel = SnakeLevelManager.shared.level(id: uuid)!
+
         return Group {
-            LevelView()
+            LevelView(level: level)
                 .previewLayout(.fixed(width: 130, height: 200))
-            LevelView()
+            LevelView(level: level)
                 .previewLayout(.fixed(width: 300, height: 200))
-            LevelView()
+            LevelView(level: level)
                 .previewLayout(.fixed(width: 500, height: 150))
         }
     }
