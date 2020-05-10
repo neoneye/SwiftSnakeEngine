6c86d4f77b209dd27c28e57bc9e9e7ed5b13c524
Simon Strandgaard <neoneye@gmail.com>
2020-05-10 13:48:22 +0200

    Extracted LevelCell.create()

--- AppMac/LevelView.swift
+++ AppMac/LevelView.swift
@@ -12,6 +12,28 @@ import EngineMac
 struct LevelCell: Identifiable {
     var id: Int
     var position: UIntVec2
+
+    static func create(level: SnakeLevel) -> [LevelCell] {
+        let size: UIntVec2 = level.size
+        var levelCellArray = [LevelCell]()
+        for y in 0..<size.y {
+            for x in 0..<size.x {
+                let flippedPosition = UIntVec2(x: x, y: size.y - 1 - y)
+                guard let cell: SnakeLevelCell = level.getValue(flippedPosition) else {
+                    continue
+                }
+                if cell == .empty {
+                    continue
+                }
+
+                let id = Int(y * size.x + x)
+                let position = UIntVec2(x: x, y: y)
+                let levelCell = LevelCell(id: id, position: position)
+                levelCellArray.append(levelCell)
+            }
+        }
+        return levelCellArray
+    }
 }
 
 struct LevelCellView: View {
@@ -65,24 +87,7 @@ struct LevelView: View {
         let uuid = UUID(uuidString: "cdeeadf2-31c9-48f4-852f-778b58086dd0")!
         let level: SnakeLevel = SnakeLevelManager.shared.level(id: uuid)!
         let size: UIntVec2 = level.size
-
-        var levelCellArray = [LevelCell]()
-        for y in 0..<size.y {
-            for x in 0..<size.x {
-                let flippedPosition = UIntVec2(x: x, y: size.y - 1 - y)
-                guard let cell: SnakeLevelCell = level.getValue(flippedPosition) else {
-                    continue
-                }
-                if cell == .empty {
-                    continue
-                }
-
-                let id = Int(y * size.x + x)
-                let position = UIntVec2(x: x, y: y)
-                let levelCell = LevelCell(id: id, position: position)
-                levelCellArray.append(levelCell)
-            }
-        }
+        let levelCellArray: [LevelCell] = LevelCell.create(level: level)
 
         return LevelGridView(rowCount: UInt(size.y), columnCount: UInt(size.x), levelCellArray: levelCellArray)
     }
