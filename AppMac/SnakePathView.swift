0c9ad0d3ed66568d537321151f9ceb40af7567de
Simon Strandgaard <neoneye@gmail.com>
2020-05-10 02:57:07 +0200

    SnakePathView now shows something that resembles a snake body.

--- AppMac/SnakePathView.swift
+++ AppMac/SnakePathView.swift
@@ -11,20 +11,77 @@ import EngineMac
 #endif
 
 struct SnakePathView: View {
+    let positions: [IntVec2]
+
     var body: some View {
         Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
+            let tileSize: CGFloat = 20
+            let halfBorderSize: CGFloat = 8
+            let halfCornerOverlap: CGFloat = 5
+            for (index, currentPosition) in positions.enumerated() {
+                if index == 0 {
+                    continue
+                }
+                let previousPosition: IntVec2 = positions[index-1]
+                let diff: IntVec2 = currentPosition.subtract(previousPosition)
+                let dxx: UInt = UInt(diff.x * diff.x)
+                let dyy: UInt = UInt(diff.y * diff.y)
+
+                var point0 = CGPoint(
+                    x: CGFloat(previousPosition.x) * tileSize,
+                    y: CGFloat(previousPosition.y) * tileSize
+                )
+                var point1 = CGPoint(
+                    x: CGFloat(currentPosition.x) * tileSize,
+                    y: CGFloat(currentPosition.y) * tileSize
+                )
+
+                if dxx > dyy {
+                    point0.y -= halfBorderSize
+                    point1.y += halfBorderSize
+                    if diff.x > 0 {
+                        point0.x -= halfCornerOverlap
+                        point1.x += halfCornerOverlap
+                    } else {
+                        point0.x += halfCornerOverlap
+                        point1.x -= halfCornerOverlap
+                    }
+                } else {
+                    point0.x -= halfBorderSize
+                    point1.x += halfBorderSize
+                    if diff.y > 0 {
+                        point0.y -= halfCornerOverlap
+                        point1.y += halfCornerOverlap
+                    } else {
+                        point0.y += halfCornerOverlap
+                        point1.y -= halfCornerOverlap
+                    }
+                }
+
+                let x0: CGFloat = min(point0.x, point1.x)
+                let y0: CGFloat = min(point0.y, point1.y)
+                let x1: CGFloat = max(point0.x, point1.x)
+                let y1: CGFloat = max(point0.y, point1.y)
+                let rect = CGRect(x: x0, y: y0, width: x1-x0, height: y1-y0)
+                path.addRect(rect)
+            }
         }
-        .fill(Color.black)
+        .fill(Color.red)
         .frame(width: 400, height: 400)
     }
 }
 
 struct SnakePathView_Previews: PreviewProvider {
     static var previews: some View {
-        SnakePathView()
+        let positions: [IntVec2] = [
+            IntVec2(x:  7, y: 10),
+            IntVec2(x: 10, y: 10),
+            IntVec2(x: 10, y:  7),
+            IntVec2(x: 13, y:  7),
+            IntVec2(x: 13, y:  9),
+            IntVec2(x: 12, y:  9),
+            IntVec2(x: 12, y: 12),
+        ]
+        return SnakePathView(positions: positions)
     }
 }
