45dbf8962cfe4fd123c6db1c71d8f7a2b0c9f8d4
Simon Strandgaard <neoneye@gmail.com>
2020-05-17 04:05:33 +0200

    RootView added.

---
+++ AppShared/RootView.swift
@@ -0,0 +1,13 @@
+// MIT license. Copyright (c) 2020 Simon Strandgaard. All rights reserved.
+import SwiftUI
+
+struct RootView: View {
+    @State var model: GameViewModel
+    @ObservedObject var levelSelectorViewModel: LevelSelectorViewModel
+    @ObservedObject var settingStore: SettingStore
+
+    var body: some View {
+        MyContentView(model: model, levelSelectorViewModel: levelSelectorViewModel)
+        .environmentObject(settingStore)
+    }
+}
