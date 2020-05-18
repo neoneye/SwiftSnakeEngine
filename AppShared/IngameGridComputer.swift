57deb99034efcf1c39bc956db15bd9bbb5e039f5
Simon Strandgaard <neoneye@gmail.com>
2020-05-18 02:48:39 +0200

    IngameGridComputer. Added padding around the level.

--- AppShared/IngameGridComputer.swift
+++ AppShared/IngameGridComputer.swift
@@ -18,17 +18,27 @@ struct IngameGridComputer {
     let gridSize: UIntVec2
     let halfCellSize: CGSize
     let cellSize: CGSize
-    let inset: CGPoint
+    let translate: CGPoint
     let tileMinSize: CGFloat
 
-    init(viewSize: CGSize, gridSize gs: UIntVec2) {
-        
+    init(viewSize originalViewSize: CGSize, gridSize originalGridView: UIntVec2) {
+        // At first I wanted to reduce wasted screen estate as much as possible.
+        // I removed the outer cells adjacent to the level edge, so that
+        // the level extends entirely to the screen edge.
+        // However this gave the impression that the content had been cropped by accident.
+        // So I have added a small amount of padding around level.
+        let viewTrim: CGFloat = 2
+        let viewTrimBothSides: CGFloat = viewTrim * 2
+        let vsx: CGFloat = max(originalViewSize.width - viewTrimBothSides, 1)
+        let vsy: CGFloat = max(originalViewSize.height - viewTrimBothSides, 1)
+        let viewSize = CGSize(width: vsx, height: vsy)
+
         // When trimming the outer cells adjacent to the edge:
         // Then it's left + right sides, so 2 cells removed in the horizontal axis.
         // Then it's top + bottom sides, so 2 cells removed in the vertical axis.
         let cellTrim: Int = Self.trimEdges ? 2 : 0
-        let gsx: Int = max(Int(gs.x) - cellTrim, 1)
-        let gsy: Int = max(Int(gs.y) - cellTrim, 1)
+        let gsx: Int = max(Int(originalGridView.x) - cellTrim, 1)
+        let gsy: Int = max(Int(originalGridView.y) - cellTrim, 1)
         let gridSize: UIntVec2 = UIntVec2(x: UInt32(gsx), y: UInt32(gsy))
         self.viewSize = viewSize
         self.gridSize = gridSize
@@ -42,21 +52,37 @@ struct IngameGridComputer {
         let cellSize: CGSize = CGSize(width: width, height: height)
         self.cellSize = cellSize
 
-        let inset: CGPoint = CGPoint(
-            x: floor((viewSize.width - width * CGFloat(gridSize.x))/2) + halfWidth,
-            y: floor((viewSize.height - height * CGFloat(gridSize.y))/2) + halfHeight
-        )
-        self.inset = inset
+        var translate = CGPoint.zero
+        do {
+            // Remaining space outside the grid size, but still inside the view size
+            translate.x += floor((viewSize.width - width * CGFloat(gridSize.x))/2)
+            translate.y += floor((viewSize.height - height * CGFloat(gridSize.y))/2)
+        }
+        do {
+            // Center in the midx midy of the cell.
+            translate.x += halfWidth
+            translate.y += halfHeight
+        }
+        do {
+            //
+            translate.x += viewTrim
+            translate.y += viewTrim
+        }
+        if Self.trimEdges {
+            // When trimming the cells near the edges, then we ignore the bottom row, and the left column.
+            translate.x -= width
+            translate.y -= height
+        }
+
+        self.translate = translate
 
         self.tileMinSize = min(cellSize.width, cellSize.height)
     }
 
     private func computePosition(x: Int, y: Int) -> CGPoint {
-        // When trimming the cells near the edges, then we ignore the bottom row, and the left column.
-        let cellOffset: Int = Self.trimEdges ? 1 : 0
         return CGPoint(
-            x: CGFloat(x - cellOffset) * cellSize.width + inset.x,
-            y: CGFloat(y - cellOffset) * cellSize.height + inset.y
+            x: CGFloat(x) * cellSize.width + translate.x,
+            y: CGFloat(y) * cellSize.height + translate.y
         )
     }
 
