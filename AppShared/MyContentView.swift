8b58bd083e6410bc917be96e6bba1aa7d83175cb
Simon Strandgaard <neoneye@gmail.com>
2020-05-20 01:32:52 +0200

    Removed dead code.

--- AppShared/MyContentView.swift
+++ AppShared/MyContentView.swift
@@ -26,138 +26,8 @@ struct MyContentView: View {
     @Environment(\.keyPublisher) var keyPublisher
     #endif
 
-    @State private var player1Dead: Bool = false
-    @State private var player2Dead: Bool = false
     @State var presentingModal = false
     var isPreview: Bool = false
-    let player1ColorAlive: Color = AppColor.player1_snakeBody.color
-    let player2ColorAlive: Color = AppColor.player2_snakeBody.color
-
-    let showDebugPanels = false
-
-    var player1Color: Color {
-        if player1Dead {
-            return player1ColorAlive.opacity(0.3)
-        } else {
-            return player1ColorAlive
-        }
-    }
-
-    var player2Color: Color {
-        if player2Dead {
-            return player2ColorAlive.opacity(0.3)
-        } else {
-            return player2ColorAlive
-        }
-    }
-
-    var debugPanel1: some View {
-        HStack {
-            Button("Dead/Alive") {
-                self.player1Dead.toggle()
-            }
-            Button("+") {
-                let length: UInt = self.model.player1Length
-                self.model.player1Length = length + 1
-            }
-            Button("-") {
-                let length: UInt = self.model.player1Length
-                if length >= 1 {
-                    self.model.player1Length = length - 1
-                }
-            }
-        }
-    }
-
-    var debugPanel2: some View {
-        HStack {
-            Button("Dead/Alive") {
-                self.player2Dead.toggle()
-            }
-            Button("+") {
-                let length: UInt = self.model.player2Length
-                self.model.player2Length = length + 1
-            }
-            Button("-") {
-                let length: UInt = self.model.player2Length
-                if length >= 1 {
-                    self.model.player2Length = length - 1
-                }
-            }
-        }
-    }
-
-    var stripeImage: some View {
-        Image("stripes")
-            .resizable(resizingMode: .tile)
-            .contrast(0.2)
-            .colorMultiply(Color(white: 0.3))
-    }
-
-    var leftSide: some View {
-        HStack(spacing: 1) {
-            VStack(alignment: .leading, spacing: 0) {
-
-                Text(model.player1Info)
-                    .padding(10)
-                    .frame(minWidth: 80, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                    .background(self.player1Color)
-                    .foregroundColor(.black)
-
-                if self.showDebugPanels {
-                    debugPanel1
-                }
-
-            }
-
-            if model.player1Length >= 1 {
-                PlayerScoreView(
-                    playerLength: $model.player1Length,
-                    color: self.player1Color
-                )
-            }
-        }
-    }
-
-    var rightSide: some View {
-        HStack(spacing: 1) {
-            if model.player2Length >= 1 {
-                PlayerScoreView(
-                    playerLength: $model.player2Length,
-                    color: self.player2Color
-                )
-            }
-
-            VStack(alignment: .leading, spacing: 0) {
-
-                Text(model.player2Info)
-                    .padding(10)
-                    .frame(minWidth: 80, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-                    .background(self.player2Color)
-                    .foregroundColor(.black)
-
-                if self.showDebugPanels {
-                    debugPanel2
-                }
-            }
-        }
-    }
-
-    var leftSideOpacity: Double {
-        if model.player1Length >= 1 {
-            return 1
-        } else {
-            return 0
-        }
-    }
-
-    var rightSideOpacity: Double {
-        if model.player2Length >= 1 {
-            return 1
-        } else {
-            return 0
-        }
-    }
 
     private var ingameView: some View {
         IngameView(
@@ -273,9 +143,6 @@ struct MyContentView: View {
             model.singleStep_botsOnly()
         case .letterZ:
             model.undo()
-//        case .letterT:
-//            let url: URL = gameState.saveTrainingData(trainingSessionUUID: self.trainingSessionUUID)
-//            trainingSessionURLs.append(url)
         case .enter:
             model.restartGame()
         case .tab:
@@ -297,31 +164,6 @@ struct MyContentView: View {
         }
     }
     #endif
-
-    private var pauseButton: some View {
-        Button(action: {
-            log.debug("pause button pressed")
-            self.presentingModal = true
-        }) {
-            Image("ingame_pauseButton_image")
-                .foregroundColor(AppColor.ingame_pauseButton.color)
-                .scaleEffect(0.6)
-                .padding(15)
-        }
-        .sheet(isPresented: $presentingModal) {
-            PauseSheetView(model: self.model, presentedAsModal: self.$presentingModal)
-        }
-    }
-
-    private var iOS_overlayWithPauseButton: some View {
-        VStack {
-            HStack {
-                pauseButton
-                Spacer()
-            }
-            Spacer()
-        }
-    }
 
     private var iOS_overlayWithHeader_inner: some View {
         // Translation between boolean <-> playerMode enum.
@@ -367,26 +209,6 @@ struct MyContentView: View {
             Spacer()
         }
         .foregroundColor(Color.black)
-    }
-
-    private var macOS_footer: some View {
-        ZStack {
-            stripeImage
-
-            HStack(spacing: 1) {
-
-                leftSide
-                    .opacity(leftSideOpacity)
-                    .frame(maxWidth: .infinity)
-
-                rightSide
-                    .opacity(rightSideOpacity)
-                    .frame(maxWidth: .infinity)
-
-            }
-
-        }
-        .frame(minWidth: 80, maxWidth: .infinity, minHeight: 80, maxHeight: 100)
     }
 
     var mainContent: some View {
