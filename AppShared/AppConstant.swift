24142b769df622d2499048634193675860546816
Simon Strandgaard <neoneye@gmail.com>
2020-05-20 01:58:37 +0200

    Removed dead code.

--- AppShared/AppConstant.swift
+++ AppShared/AppConstant.swift
@@ -19,33 +19,9 @@ struct AppConstant {
 	}
 	static let mode: Mode = .production
 
-	enum SnakeGameTheme {
-		case theme1
-		case theme2
-	}
-	static let theme: SnakeGameTheme = .theme1
-
-	static let tileSize: CGFloat = 100
-
 	static let ignoreRepeatingKeyDownEvents = true
 
-    enum GameInitialStepMode {
-        // Similar to most video players that begins playing right away.
-        // Immediately after selecting the level, the bot begins moving around.
-        case production_stepForwardContinuously
-
-        // Similar to single stepping with a debugger.
-        // Nothing happens after selecting the level. The developer has to press F6 to single step.
-        case doNothing
-    }
-    static let gameInitialStepMode: GameInitialStepMode = .production_stepForwardContinuously
-//    static let gameInitialStepMode: GameInitialStepMode = .doNothing
-
 	static let saveTrainingData = false
-
-    struct SpriteKit {
-        static let showDeveloperInfo = false
-    }
 
     struct Dashboard {
         static let url = URL(string: "http://localhost:4000/")!
