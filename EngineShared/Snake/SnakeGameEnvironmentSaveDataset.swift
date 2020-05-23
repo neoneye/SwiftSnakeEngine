1201eda2f4c953204f65d95154af70acdc31e989
Simon Strandgaard <neoneye@gmail.com>
2020-05-24 01:56:52 +0200

    SnakeGameEnvironmentSaveDataset no longer saves each step to a temporary file. It's only the end result that is saved as a file.

--- EngineShared/Snake/SnakeGameEnvironmentSaveDataset.swift
+++ EngineShared/Snake/SnakeGameEnvironmentSaveDataset.swift
@@ -4,35 +4,42 @@ import Foundation
 public class SnakeGameEnvironmentSaveDataset: SnakeGameEnvironment {
     private let wrapped: SnakeGameEnvironment
     private var trainingSessionUUID: UUID
-    private var trainingSessionURLs: [URL]
+    private var stepArray: [SnakeDatasetStep]
+    private var level: SnakeLevel?
 
     public init(wrapped: SnakeGameEnvironment) {
-        self.trainingSessionUUID = UUID()
-        self.trainingSessionURLs = []
         self.wrapped = wrapped
+        self.trainingSessionUUID = UUID()
+        self.stepArray = []
+        self.level = nil
     }
 
     public func reset() -> SnakeGameState {
         trainingSessionUUID = UUID()
-        trainingSessionURLs = []
-        return wrapped.reset()
+        stepArray = []
+        let gameState: SnakeGameState = wrapped.reset()
+        stepArray.append(gameState.toSnakeDatasetStep())
+        level = gameState.level
+        return gameState
     }
 
     public func undo() -> SnakeGameState? {
-        // IDEA: deal with undo and discard mis-steps.
-        //
         // In a game with human players, the 'undo' function is often used.
         // Ideally the dataset file should contain all the data. Both steps and mis-steps.
-        // For now I want to eliminate all the mis-steps. It will appear as if the human
+        // For now I'm eliminating all the mis-steps. It will appear as if the human
         // did do a much better game without any mis-steps at all.
         // In the future I may choose to include all mis-steps in the result file.
-        return wrapped.undo()
+        guard let gameState: SnakeGameState = wrapped.undo() else {
+            return nil
+        }
+        // Discard the mis-step.
+        _ = stepArray.popLast()
+        return gameState
     }
 
     public func step(action: SnakeGameAction) -> SnakeGameState {
         let gameState: SnakeGameState = wrapped.step(action: action)
-        let url: URL = gameState.saveTrainingData(trainingSessionUUID: self.trainingSessionUUID)
-        trainingSessionURLs.append(url)
+        stepArray.append(gameState.toSnakeDatasetStep())
 
         // The game is over when both players are dead
         let player1Alive: Bool = gameState.player1.isInstalledAndAlive
@@ -46,11 +53,15 @@ public class SnakeGameEnvironmentSaveDataset: SnakeGameEnvironment {
     }
 
     public func postProcess() {
+        guard let level: SnakeLevel = self.level else {
+            log.error("Expected level to be non-nil, but got nil. Maybe reset() was never invoked. Cannot generate a dataset file.")
+            return
+        }
+        let levelDataset: SnakeDatasetLevel = level.toSnakeDatasetLevel()
+        let processor = PostProcessTrainingData(level: levelDataset, stepArray: stepArray)
+        processor.saveToTempoaryFile(trainingSessionUUID: trainingSessionUUID)
+
         // IDEA: Determine the winner: the longest snake, or the longest lived snake, or a combo?
         // IDEA: pass on which player won/loose.
-        PostProcessTrainingData.process(
-            trainingSessionUUID: self.trainingSessionUUID,
-            urls: self.trainingSessionURLs
-        )
     }
 }
