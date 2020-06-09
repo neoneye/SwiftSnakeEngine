4952002cff93a427639e272d380c08b08b98e75d
Simon Strandgaard <neoneye@gmail.com>
2020-06-09 03:47:34 +0200

    9x9 grid with obstacles surrounding the head.

--- EngineShared/Bot/SnakeBot8.swift
+++ EngineShared/Bot/SnakeBot8.swift
@@ -29,6 +29,44 @@ public class SnakeBot8: SnakeBot {
             return SnakeBot8()
         }
 
+        let headPosition: IntVec2 = player.snakeBody.head.position
+
+        // 9x9 grid with obstacles surrounding the head
+        let grid = Array2<Bool>(size: UIntVec2(x: 9, y: 9), defaultValue: false)
+
+        // Draw the level walls as obstacles
+        for y: Int32 in 0...8 {
+            for x: Int32 in 0...8 {
+                let position: IntVec2 = headPosition.offsetBy(dx: x-4, dy: y-4)
+                guard let cell: SnakeLevelCell = level.getValue(position) else {
+                    grid.setValue(true, at: IntVec2(x: x, y: y))
+                    continue
+                }
+                if cell == .wall {
+                    grid.setValue(true, at: IntVec2(x: x, y: y))
+                }
+            }
+        }
+
+        // Draw players as obstacles
+        var playerPositionSet: Set<IntVec2> = player.snakeBody.positionSet()
+        if oppositePlayer.isInstalledAndAlive {
+            playerPositionSet.formUnion(oppositePlayer.snakeBody.positionSet())
+        }
+        for y: Int32 in 0...8 {
+            for x: Int32 in 0...8 {
+                let position: IntVec2 = headPosition.offsetBy(dx: x-4, dy: y-4)
+                if playerPositionSet.contains(position) {
+                    grid.setValue(true, at: IntVec2(x: x, y: y))
+                }
+            }
+        }
+
+        let gridString = grid.flipY.format(columnSeparator: " ") { (value, position) in
+            value ? "*" : "-"
+        }
+        log.debug("grid: \(gridString)")
+
         let pendingMovement: SnakeBodyMovement = .moveForward
 
         return SnakeBot8(
